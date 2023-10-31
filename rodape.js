bfd4b84ef1da8ac4614b96de0be44863d89844a9
BrunoDomingosH <124690176+BrunoDomingosH@users.noreply.github.com>
2023-10-31 12:21:10 -0300

    Add files via upload

---
+++ rodape.js
@@ -0,0 +1,32 @@
+function Aparecer()
+    {
+        lateral.classList.add('animar');
+        lateral2.classList.remove('animar2');
+    }
+
+    function Fechar()
+    {
+        lateral.classList.remove('animar');
+    }
+
+
+
+
+
+
+
+
+
+    function Aparecer2()
+    {
+        lateral2.classList.add('animar2');
+        lateral.classList.remove('animar');
+       
+    }
+
+    function Fechar2()
+    {
+        lateral2.classList.remove('animar2');
+    }
+
+    
